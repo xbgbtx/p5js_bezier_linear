69c74bb78d15153b207ca81dd977b631ef286e7f
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-11-18 18:02:55 +0000

    Support for dragging point t

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-let p0, p1, t, tSlider;
+let p0, p1, t, pt;
 
 function setup() 
 {
     createCanvas(400,400);
     p0 = createVector(100,300);
     p1 = createVector(300,100);
+    pt = createVector(0,0);
+
     t = 0.5;
-    tSlider = createSlider(0, 1,t, 0.05);
-    tSlider.position(10,10);
 
     add_interaction_cbs();
 }
@@ -23,11 +23,53 @@ function add_interaction_cbs ()
     MS.add_interaction_cb ( 
         MS.Interactions.drag_points ( [p0, p1], point_drag )
     );
+
+    let t_drag = (p) =>
+    {
+        t = line_t_nearest_point ( p0, p1, createVector(mouseX,mouseY) );
+    };
+
+    MS.add_interaction_cb ( 
+        MS.Interactions.drag_points ( [pt], t_drag )
+    );
+}
+
+//Return vector2 (x,y) on line segment at time t
+function line_point ( a, b, t )
+{
+    return p5.Vector.add( 
+        p5.Vector.mult(a, 1-t), 
+        p5.Vector.mult(b, t)
+    );
+}
+
+//Returns t on line a,b nearest to point p
+function line_t_nearest_point ( a, b, p )
+{
+    let v = p5.Vector.sub(b, a);
+    let u = p5.Vector.sub(a, p);
+    let vu = v.x*u.x + v.y*u.y;
+    let vv = v.x**2 + v.y**2;
+
+    let lt = -vu/vv;
+
+    lt = constrain(lt,0,1);
+
+    return lt;
+}
+
+function update_pt()
+{
+    pt_ = line_point ( p0, p1, t );
+    pt.x = pt_.x;
+    pt.y = pt_.y;
 }
 
 function draw() 
 {
     MS.handle_interaction ();
+
+    update_pt();
 
     background(MS.colors.background);
 
@@ -46,15 +88,9 @@ function draw()
         color : MS.colors.foreground[2],
     });
     
-    t=tSlider.value();
-    
-    pt = p5.Vector.add( 
-        p5.Vector.mult(p0, 1-t), 
-        p5.Vector.mult(p1, t)
-    );
     MS.renderPoint(pt,
     {
-        label : `t=${t}`,
+        label : `t=${t.toFixed(2)}`,
         color : MS.colors.foreground[3],
     });
 }
